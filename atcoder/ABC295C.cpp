505f090ebaa5bb1b7c7fec7b6ec7a4416ce7e6df
Choi-gang <gang2geon@gmail.com>
2026-07-07 09:59:13 +0900

    [AC]7/7 updated

--- atcoder/ABC295C.cpp
+++ atcoder/ABC295C.cpp
@@ -6,11 +6,12 @@ int main()
     int result = 0;
     std::cin >> N;
     std::unordered_map<long long, int> pairs;
+    // 색의 종류가 많아서 vector 보다는 map, long long
 
     for(int i = 0; i < N; i++) {
         int color;
         std::cin >> color;
-        pairs[color]++;
+        pairs[color]++; // 색 종류 개수구하기
     }
 
     for(auto &c : pairs) {
